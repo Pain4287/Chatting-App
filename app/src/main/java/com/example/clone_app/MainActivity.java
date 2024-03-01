ff129b1dd4e01cba1c64d29d6b8cde9ba0658fe9
Rajveer <panchalrajveer96@gmail.com>
2024-03-01 11:05:02 +0530

    Chatting app repo

--- app/src/main/java/com/example/clone_app/MainActivity.java
+++ app/src/main/java/com/example/clone_app/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.clone_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView cht,srch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cht = findViewById(R.id.chat1);
+        srch = findViewById(R.id.search);
+
+        cht.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,Chat.class );
+                startActivity(i);
+            }
+        });
+
+        srch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sr = new Intent(MainActivity.this, Search.class);
+                startActivity(sr);
+            }
+        });
+
     }
 }
